b67f3d73ef9aa0c364953677f422a44978ff89f8
programadorisgod <catapiascastro@gmail.com>
2023-12-11 16:45:15 -0500

    -mcommit

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,23 @@
 <script>
-
     import Input from "./Input.svelte";
+
     let accept = false;
+
     export let persons = [
         {name: "John", age: 20},
         {name: "Jane", age: 30},
         {name: "Joe", age: 40},
     ];
+
 </script>
 
 
 {#each persons as {name, age}}
+
     <h2>{name}</h2>
+
     <span>{age}</span>
+
 {/each}
 
 <Input></Input>
@@ -20,11 +25,11 @@
 
 <label for="">
 <input type="checkbox" bind:checked={accept}/>
-Accept terms and conditions
+    Accept terms and conditions
 </label>
 
 {#if accept}
      <p>Accepted</p>
 {:else }
- <p style="color: re;">No accept</p>
+    <p style="color: red;">No accept</p>
 {/if}
